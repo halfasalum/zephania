9d17ddc332d0e5ebb2b2f1173405aa543a4ab627
ahbabrasul <ahbabrasul@icloud.com>
2026-01-13 14:30:14 +0300

    routing seutp complete

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,13 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import base from '@/views/base.vue'
+import home from '@/views/home.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
      {
       path: "/",
-      name: "Home",
       component: base,
+      children: [
+        {
+          path: "",
+          name: "Home",
+          component: home,
+        },
+      ],
     },
   ],
 })
